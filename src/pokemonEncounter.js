cb81195ccdfe799f4aebb4304aa0c570904bb497
John Niduelan <jeniduelan@gmail.com>
2024-09-05 20:14:19 +1000

    feat: render pokemon name

--- src/pokemonEncounter.js
+++ src/pokemonEncounter.js
@@ -1,28 +1,44 @@
+let pokemonRenderArea = document.getElementById("pokemonEncounterArea");
+
+function renderPokemonData(pokemonData){
+	pokemonRenderArea.innerText = pokemonData.name;
+}
+
 
 async function getPokemon(){
-    console.log("Getting Pokemon now!");
-    // Hardcoded for development, replace "pickachu" with a random number
-    // random number is ID from 1 to 1025
-    let apiResponse = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu");
-    let apiData = await apiResponse.json();
+	console.log("Getting Pokemon now!");
 
-    //
-    let pokemonName = apiData.name;
+	// Hardcoded for development, replace "pikachu" with a random number
+	// random number is ID from 1 to 1025
+	let apiResponse = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu");
+	let apiData = await apiResponse.json();
 
-    return {
-        name: apiData.name,
-        types: apiData.types,
-        image: apiData.sprites.other.home.front_default,
-        sound: apiData.cries.latest
-    };
+	// console.log(apiData);
+	// Name, Types, Image, Sound
+	// let pokemonName = apiData.name;
+	// return pokemonName;
+
+	return {
+		name: apiData.name,
+		types: apiData.types,
+		image: apiData.sprites.other.home.front_default,
+		sound: apiData.cries.latest
+	}
 }
 
 let encounterButton = document.getElementById("pokemonEncounterButton");
 
-encounterButton.addEventListener("clock", getPokemon);
-// encounterButton.addEventListener("click", (even) => getPokemon(event));
-encounterButton.addEventListener("click", async (even) => {
-    console.log("some block of code in the event listener");
-    let pokemonResult = getPokemon();
-    console.log(pokemonResult);   
+// encounterButton.addEventListener("click", getPokemon);
+// encounterButton.addEventListener("click", (event) => getPokemon(event));
+encounterButton.addEventListener("click", async (event) => {
+
+	console.log("Some block of code in the event listener");
+
+	let pokemonResult = await getPokemon();
+
+	console.log(pokemonResult);
+
+	renderPokemonData(pokemonResult);
 });
+
+
